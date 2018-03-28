fa6637cad5c7f3259525fcdae8f35063592d40b4
Isaac Ballas <isaacthedeveloper@gmail.com>
2018-03-28 13:41:20 -0400

    Initial Commit

---
+++ Gestures/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gestures
+//
+//  Created by Isaac Ballas on 2018-03-28.
+//  Copyright © 2018 Isaac Ballas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
